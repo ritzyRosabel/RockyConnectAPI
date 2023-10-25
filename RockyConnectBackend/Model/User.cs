3d33033a97ae615e8b66979b5a1ce32cc1577408
Rosabel <rosabelolugbenga@yahoo.com>
2023-10-25 08:07:45 -0500

    update password

--- RockyConnectBackend/Model/User.cs
+++ RockyConnectBackend/Model/User.cs
@@ -34,10 +34,10 @@ namespace RockyConnectBackend.Model
     }
     public class UserUpdateRequest
     {
-        public required string FirstName { get; set; }
-        public required string LastName { get; set; }
-        public required string PhoneNumber { get; set; }
-        public required string Password { get; set; }
+        public  string? FirstName { get; set; }
+        public  string? LastName { get; set; }
+        public  string ?PhoneNumber { get; set; }
+        public  string? Password { get; set; }
         public required string Email { get; set; }
     }
         public class LoginUser
